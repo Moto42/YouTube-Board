fdb0cb7222ebc21cee6ccfbe4b85234c8849bd49
Moto42 <moto42@gmail.com>
2019-02-01 15:24:03 -0600

    Added Header content and Formatting

--- youtube-board/src/Header/Header.js
+++ youtube-board/src/Header/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import icon_youtube from "../images/youtube-icon.png";
+import "./Header.css"
 
 function Header() {
   return (
     <header>
-      THIS IS THE HEADER
+      <img src={icon_youtube} alt="YouTube" className="yt-icon" />
+      <p className="logoText">
+        YouTube Soundboard
+      </p>
     </header>
   )
 };
